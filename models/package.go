2bc0197bd2e7bd8ff65ca59977c7e11d6ba3d985
최다루한 <choidaruhan@choidaruhan-macbook.local>
2025-06-12 17:31:30 +0900

    fix: enum('등록됨','A차운송중','투입됨','B차운송중','완료됨'

--- models/package.go
+++ models/package.go
@@ -9,7 +9,7 @@ type Package struct {
 	PackageType   string    `json:"package_type" gorm:"column:package_type;type:varchar(50);not null"`
 	RegionID      string    `json:"region_id" gorm:"column:region_id;type:char(3);not null"`
 	Region        Region    `json:"-" gorm:"foreignKey:RegionID;references:RegionID"`
-	PackageStatus string    `json:"package_status" gorm:"column:package_status;type:enum('registered','A_transport','input','B_transport','completed')"`
+	PackageStatus string    `json:"package_status" gorm:"column:package_status;type:enum('등록됨','A차운송중','투입됨','B차운송중','완료됨';default:'등록됨'"`
 	RegisteredAt  time.Time `json:"registered_at" gorm:"column:registered_at;type:datetime;not null;default:CURRENT_TIMESTAMP"`
 }
 
